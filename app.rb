b06b3c4d4aae5a7f937db8137d72ed5808c15305
Mark Hernandez <m.hern1415@gmail.com>
2019-10-10 23:04:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  
+  get '/reversename/:name' do
+    @name = params[:name]
+    erb :reversename
+  end
+  
+  get '/square/:number' do
+    @number = params[:number].to_i
+    erb :square
+    
+  end
+  
+  get '/say/:number/:phrase'
 
 end
